24e2e15e614e45bc9f9bdf16e95d939c8f8b0f24
Adrian Conlon <adrian.conlon@gmail.com>
2016-09-09 20:18:32 +0100

    Switch to using the IntelliJ code formatting.
    Signed-off-by: Adrian Conlon <adrian.conlon@gmail.com>

--- yoti.go
+++ yoti.go
@@ -1,10 +1,8 @@
-/* vim: set tabstop=4: */
-
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 //////////////////
@@ -26,29 +24,29 @@ func RetrieveHandler(w http.ResponseWriter, r *http.Request) {
 //////////////////
 
 func PostOnly(h handler) handler {
-    return func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == "POST" {
-            h(w, r)
-            return
-        }
-        http.Error(w, "post only", http.StatusMethodNotAllowed)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			h(w, r)
+			return
+		}
+		http.Error(w, "post only", http.StatusMethodNotAllowed)
+	}
 }
 
 func GetOnly(h handler) handler {
-    return func(w http.ResponseWriter, r *http.Request) {
-        if r.Method == "GET" {
-            h(w, r)
-            return
-        }
-        http.Error(w, "get only", http.StatusMethodNotAllowed)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "GET" {
+			h(w, r)
+			return
+		}
+		http.Error(w, "get only", http.StatusMethodNotAllowed)
+	}
 }
 
 //////////////////
 
 func main() {
-    http.HandleFunc("/store", PostOnly(StoreHandler))
-    http.HandleFunc("/retrieve", GetOnly(RetrieveHandler))
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/store", PostOnly(StoreHandler))
+	http.HandleFunc("/retrieve", GetOnly(RetrieveHandler))
+	http.ListenAndServe(":8080", nil)
 }
