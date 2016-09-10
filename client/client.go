688b94c11c4e91152cc0a3463fcdca7be1dc3bbb
Adrian Conlon <adrian.conlon@gmail.com>
2016-09-10 11:26:54 +0100

    Add skeletal implementation of client interface.

---
+++ client/client.go
@@ -0,0 +1,23 @@
+package client
+
+func (c Client) Store(id, payload []byte) (aesKey []byte, err error) {
+
+	var key []byte
+	aesKey = key
+
+	var problem error
+	err = problem
+
+	return
+}
+
+func (c Client) Retrieve(id, aesKey []byte) (payload []byte, err error) {
+
+	var result []byte
+	payload = result
+
+	var problem error
+	err = problem
+
+	return
+}
